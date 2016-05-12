c75b800f0e7992ebdc2894467bb9bdf08c8c507e
Mario Alvarez <machimocho@gmail.com>
2016-05-12 00:02:13 -0500

    Desarrollo ágil con AngularJS Página 40

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,6 +8,8 @@ var gulp = require('gulp'),
 
  var stylus = require('gulp-stylus'),
   nib = require('nib');
+  var inject = require('gulp-inject');
+  var wiredep = require('wiredep').stream;
 // Servidor web de desarrollo
 gulp.task('server', function() {
  connect.server({
@@ -41,11 +43,32 @@ gulp.task('html', function() {
  gulp.src('./app/**/*.html')
  .pipe(connect.reload());
 });
+
+// Busca en las carpetas de estilos y javascript los archivos que hayamos creado
+// para inyectarlos en el index.html
+gulp.task('inject', function() {
+ var sources = gulp.src(['./app/scripts/**/*.js','./app/stylesheets/**/*.css'], { read: false});
+ return gulp.src('index.html',{cwd:'./app'})
+ .pipe(inject(sources,{ ignorePath: '/app' }))
+ .pipe(gulp.dest('./app'));
+});
+
+
+
+
+// Inyecta las librerias que instalemos vía Bower
+gulp.task('wiredep', function () {
+ gulp.src('./app/index.html')
+ //.pipe(wiredep({directory: './app/lib' }))
+ .pipe(gulp.dest('./app'));
+});
+
 // Vigila cambios que se produzcan en el código
 // y lanza las tareas relacionadas
 gulp.task('watch', function() {
  gulp.watch(['./app/**/*.html'], ['html']);
- gulp.watch(['./app/stylesheets/**/*.styl'], ['css']);
- gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint']);
+ gulp.watch(['./app/stylesheets/**/*.styl'], ['css', 'inject']);
+ gulp.watch(['./app/scripts/**/*.js', './Gulpfile.js'], ['jshint', 'inject']);
+ gulp.watch(['./bower.json'], ['wiredep']);
 });
-gulp.task('default', ['server', 'watch']);
+gulp.task('default', ['server', 'inject', 'wiredep', 'watch']);
